43b1ce545f72fee51c7af0cc5fcbac8235053b00
chrisstruong <chrisstruong@gmail.com>
2023-03-11 18:57:20 -0500

    Finish matrix traversal

--- matrixTraversal.js
+++ matrixTraversal.js
@@ -1,18 +1,78 @@
+function getMaxEnergy(mat) {
+    // Description: given a 4x4 matrix mat, the initial energy is 100. There are three ways to move when going the next row. j, j-1, j+1. As going the row, the energy will decrease by by the value of the row stepped on. The final energy can be negative.
+    // Input:
+    // + mat : a multi-dimensional array. The format is mat[i][j]
+    // Output: the maximum energy left
+    // Thinking: 
+    // 1) I will start at the first row. There are 4 starting point I need to track. mat[0][0], mat[0][1], mat[0][2], mat[0][3]. 
+    // 2) When going next row (i=1), there are three options for j: j-1, j, or j+1. Every element in the row can go with j, but at the beginning and the end, the options are j,j+1, and j, j-1 respectivelly. We have to track the current value j so as to give the approriate answer
+    // 3) When we can determine the position of j (j!==undefined), then choose the smallest energy in the row to calculate
+    // 4) This process is keep going and going until we reach the last row
 
-// Description: given a 4x4 matrix mat, the initial energy is 100. There are three ways to move when going the next row. j, j-1, j+1. As going the row, the energy will decrease by by the value of the row stepped on. The final energy can be negative.
-// Input:
-// + mat : a multi-dimensional array
-// Output: the maximum energy left
-// Thinking: I will hardcode since there is a stable 4x4 matrix. Start at i =0, then i =1, i =2, i = 3. When I take a look closely, there is a limit of movement of j. Not every j has the movement like (j, j-1, j+1).
-// Ex: 1) When i = 0 (start at index 0), the next j will be at j or j+1). Can not be j-1 because the value will be undefined
-// 2) when i = 3, the next j will be at j or j - 1.
-// 3) Only at i=1, and i = 2, the j can be at all three options: j-1, j, j+1. 
-// I will store 4 value in relation to the start of row (start at i=0, j=0, or i=0, j=1, or i=0, j=1, or i=0, j=2, or j=0, i=3)
-// Steps:
-// 1) Declear a initial energy variable, and an array to store minimum wasted energy each calculation
-// 2) First calculation based on the start i=0, j = 0. Next caculation will be the same and so on like j=1, 2, and 3
-// 3) At each new move, I will choose smallest energy to minus the initial energy
-// 4) At the end, we will have the four best minimum wasted energy (energy still have the highest energy left at the last row) in an array.
-// 5) Return the biggest energy left in that array
+    // Steps:
+    // 1) get the minEnergy will return the maxEnergy left => Declare the minEnergy, n (mat.length), let energy = 100
+    // 2) Use two for loops for multi-dimensional array
+    // 3) if left (j-1), center (j), right (j+1) are not undefined, then take smallest value among these. Otherwise only calculating not undefined variables only
+    // 4) Declare currEnergy to keep update the energy left after each row
+    // 5) set currEnergy = minEnergy and then return minEnergy
 
-func
+
+    // Step 1:
+    let minEnergy
+    const n = mat.length
+    let energy = 100
+
+    // Step 2:
+    for (let j = 0; j < n; j++) {
+        let i = 0
+        let currEnergy = energy - mat[i][j]
+        let k = j
+        for (i = 1; i < n; i++) {
+            // Step 3:
+            const left = (k > 0) ? mat[i][k - 1] : +Infinity
+            const center = mat[i][k]
+            const right = (k < n - 1) ? mat[i][k + 1] : +Infinity
+            const minValue = Math.min(left, center, right)
+            if (minValue === left) k--
+            else if (minValue === right) k++
+            //  Step 4:
+            currEnergy -= minValue
+        }
+        minEnergy = currEnergy
+    }
+    // Step 5:
+    return minEnergy
+
+    // Time complexity: O(n^2) as there is nested loops
+    // Space complexity: O(1) as the return is a int
+}
+
+let input = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+]
+console.log(getMaxEnergy(input))
+
+
+// Cheating version for matrixTraversal
+function cheatingVersion(mat) {
+    // Since we can go columly, sort each row in the array from smallest to biggest. Then take 100 - first element of each row. hihihi! 
+    let initialEnergy = 100
+    for (let i = 0; i < mat.length; i++) {
+        mat[i].sort((a,b)=> a-b)
+    }
+    return initialEnergy-mat[0][0]-mat[1][0]-mat[2][0]-mat[3][0]
+
+    
+}
+
+let input2 = [
+    [4, 3, 2, 1],
+    [8, 7, 6, 5],
+    [12, 11, 10, 9],
+    [16, 15, 14, 13]
+]
+console.log(cheatingVersion(input))
+
